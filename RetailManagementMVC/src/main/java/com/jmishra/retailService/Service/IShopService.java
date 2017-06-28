b6d65ec367f0bda4cb805bf42de9a2188aff6f4a
mishraj88 <jyotirmay.cot@gmail.com>
2017-06-28 22:21:32 +0530

    Add files via upload

--- RetailManagementMVC/src/main/java/com/jmishra/retailService/Service/IShopService.java
+++ RetailManagementMVC/src/main/java/com/jmishra/retailService/Service/IShopService.java
@@ -1,15 +1,27 @@
 package com.jmishra.retailService.Service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.jmishra.retailService.model.Shop;
 
 interface IShopService {
 
-	List<Shop> getAllShops();
+	/*
+	 * This method's implementation returns all the shops from the inventory
+	 */
+	Map<String,Shop> getAllShops();
+	
+	/*
+	 * This methods implementation would return details for a particular shop
+	 */
 	Shop getAShopDetails(String shopId);
+	
+	//This method implementation would add a new Shop to the Shop inventory
 	Shop addShop(Shop newShopDetails);
+	//This shop would update the details of a shop with the provided Shop ID
 	Shop updateShop(String shopId, Shop updatedShopDetails);
+	//This method is called by the controller to remove a Shop from the inventory
 	Shop removeShop(String shopId);
 	
 }
--- RetailManagementMVC/src/main/java/com/jmishra/retailService/Service/ShopServiceImpl.java
+++ RetailManagementMVC/src/main/java/com/jmishra/retailService/Service/ShopServiceImpl.java
@@ -2,67 +2,127 @@ package com.jmishra.retailService.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.jmishra.retailService.model.Address;
 import com.jmishra.retailService.model.Shop;
 import org.springframework.stereotype.Service;
 
-
+/*
+ * This is a Service class which basically is responsible for creating Shop class' Object
+ * I am using a HashMap<String shop_id, Shop shopObj> as an in memory internal storage
+ * as I was having some trouble resolving gradle dependency for apache derby database
+ * This class also has all the rootLevel methods which would directly interact with the dataStructure
+ * and pull out or execute the requested Operation.
+ */
 
 @Service
 public class ShopServiceImpl implements IShopService {
 
-	private List<Shop> shopList = new ArrayList<Shop>(Arrays.asList(new Shop("MyFashionStop", new Address("15A","Green Park Market","New Delhi")),
-			new Shop("MyShoeStop", new Address("11A","South Extension Market","New Delhi")),
-			new Shop("MyGeekyStop", new Address("15/26-PoketA","Nehru Place","New Delhi")),
-			new Shop("MyGameStop", new Address("209B","Cannaught Place","New Delhi"))));
-	
-	
+
+	private static HashMap<String, Shop> shopMap = new HashMap<String, Shop>();
+
+	static{
+
+		try {
+			Shop shop1 = new Shop("MyFashionStop", new Address("15A","Green Park","New Delhi"));
+
+			Shop shop2=new Shop("MyShoeStop", new Address("11A","South Extension","New Delhi"));
+			Shop shop3=new Shop("MyGeekyStop", new Address("15/26","Nehru Place","New Delhi"));
+			Shop shop4= new Shop("MyGameStop", new Address("209B","Connaught Place","New Delhi"));	
+
+			shopMap.put(shop1.getId(), shop1);
+			shopMap.put(shop2.getId(), shop2);
+			shopMap.put(shop3.getId(), shop3);
+			shopMap.put(shop4.getId(), shop4);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.jmishra.retailService.Service.IShopService#getAllShops()
+	 */
 	@Override
-	public List<Shop> getAllShops() {
-		synchronized(shopList){
-			return shopList;
+	public Map<String, Shop> getAllShops() {
+		synchronized(shopMap){
+			return shopMap;
 		}
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.jmishra.retailService.Service.IShopService#getAShopDetails(java.lang.String)
+	 */
 
 	@Override
 	public Shop getAShopDetails(String shopId) {
-		synchronized(shopList){
-			return shopList.stream().filter(t -> t.getId().equals(shopId)).findAny().get();
+		synchronized(shopMap){
+			//return shopList.stream().filter(t -> t.getId().equals(shopId)).findAny().get();
+			if(shopMap.containsKey(shopId)){
+				return shopMap.get(shopId);
+			}
+			else return null;
 		}
 	}
-
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.jmishra.retailService.Service.IShopService#addShop(com.jmishra.retailService.model.Shop)
+	 */
+	
 	@Override
 	public Shop addShop(Shop newShopDetails) {
-		synchronized(shopList){
-			shopList.add(newShopDetails);
+		synchronized(shopMap){
+
+			shopMap.put(newShopDetails.getId(), newShopDetails);
+
 		}
 		return newShopDetails;
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.jmishra.retailService.Service.IShopService#updateShop(java.lang.String, com.jmishra.retailService.model.Shop)
+	 */
+	
 
 	@Override
 	public Shop updateShop(String shopId, Shop updatedShopDetails) {
-		Shop oldShopDetails;
-		synchronized(shopList){
-			oldShopDetails = shopList.stream().filter(t -> t.getId().equals(shopId)).findFirst().get();
-		
-			for(int i=0; i<shopList.size(); i++){
-				if(shopList.get(i).getId().equals(shopId))
-					shopList.set(i, updatedShopDetails);
+		Shop oldShopDetails = null;
+		synchronized(shopMap){
+			if(shopMap.containsKey(shopId)){
+				oldShopDetails = shopMap.get(shopId);
 			}
+
+			shopMap.put(shopId, updatedShopDetails);
+
 		}
 		return oldShopDetails;
-		
+
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.jmishra.retailService.Service.IShopService#removeShop(java.lang.String)
+	 */
 
 	@Override
 	public Shop removeShop(String shopId) {
 		Shop oldShopDetails;
-		synchronized(shopList){
-		oldShopDetails = shopList.stream().filter(t -> t.getId().equals(shopId)).findFirst().get();
-		
-		shopList.removeIf(t -> t.getId().equals(shopId));
+		synchronized(shopMap){
+			oldShopDetails=shopMap.get(shopId);
+
+			shopMap.remove(shopId);
 		}
 		return oldShopDetails;
 	}
